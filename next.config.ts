a7227a09e829ca4c02a8c993b8ec7d4027267ce3
Nnamdi Emmanuel Dike <emmanueldike275@gmail.com>
2025-05-05 05:43:59 +0100

    Remove swcMinify option from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
