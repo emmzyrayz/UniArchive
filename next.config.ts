f7de568a079a078ae3d164819d07c372641141b3
Nnamdi Emmanuel Dike <emmanueldike275@gmail.com>
2025-05-05 05:32:39 +0100

    Add swcMinify and reactStrictMode options

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -21,6 +23,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  
 };
 
 export default nextConfig;
